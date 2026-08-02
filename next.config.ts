47b38dac94f02af7fcc2b422483a0e7af12debe9
Prince <148641962+Taohid-art@users.noreply.github.com>
2026-08-03 03:22:02 +0600

    Fix formatting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: ".next-build",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
     ],
   },
